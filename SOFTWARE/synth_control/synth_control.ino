0f372ab3cca8da2451213651897425cf5d780562
B4Rkhausen <orellgarten@gmx.de>
2016-04-09 21:09:34 +0200

    added untested ADSR support

--- SOFTWARE/synth_control/synth_control.ino
+++ SOFTWARE/synth_control/synth_control.ino
@@ -21,36 +21,103 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 #define CV_OUT_VCO 10
 #define CV_OUT_VCA 11
 #define CV_OUT_VCF 12
-#define SUSTAIN_VOLTAGE 13
+#define ADSR 13
+/**
+ * machine state    description
+ *  0               IDLE
+ *  1               NoteOn
+ *  2               NoteOff
+ */
+static int machine_state = 0;
+
+void writeADSR(byte state, int a, int d, int s, int r, int v, int l) 
+{
+
+  /**
+   *    state   mode
+   *    1       a -> attack
+   *    2       d -> decay
+   *    3       s -> sustain
+   *    4       r -> release
+   *    
+   *    v -> velocity
+   *    l -> sustain-level
+   */
+  
+  static int PWM_level = 0;
+  
+  switch(state){
+    case 1:
+      for(int i = 0; i < 500; i++) {
+        PWM_level = (v/a)*i;
+        analogWrite(ADSR, PWM_level);
+        delay(1);
+      }
+    break;
+    case 2:
+      for(int i = 0; i < 500; i++) {
+       PWM_level = v - ((v-s)/d)*i;
+       analogWrite(ADSR, PWM_level);
+       delay(1);
+      }
+    break;
+    case 3:
+      PWM_level = a*l;
+      analogWrite(ADSR, PWM_level);
+    break;
+    case 4:
+      for(int i = 0; i < 500; i++) {
+        PWM_level = a*l - ((a*l)/r)*i;
+        analogWrite(ADSR, PWM_level);
+        delay(1);
+      }
+    break;
+    default: break;
+  }
+}
+
 
 void handleNoteOn(byte channel, byte pitch, byte velocity)
 {
-  // clarify parameters?!
+  machine_state = 1;
+  int a_time = analogRead(ATTACK_TIME);
+  int d_time = analogRead(DECAY_TIME);
+  int s_level = analogRead(SUSTAIN_LEVEL);
+  int r_time = analogRead(RELEASE_TIME);
+
+  writeADSR(1, a_time, d_time, s_level, r_time, velocity, a_time*s_level);
+  writeADSR(2, a_time, d_time, s_level, r_time, velocity, a_time*s_level);
+  writeADSR(3, a_time, d_time, s_level, r_time, velocity, a_time*s_level);
 }
 
 void handleNoteOff(byte channel, byte pitch, byte velocity)
 {
+  int a_time = analogRead(ATTACK_TIME);
+  int d_time = analogRead(DECAY_TIME);
+  int s_level = analogRead(SUSTAIN_LEVEL);
+  int r_time = analogRead(RELEASE_TIME);
   
+  writeADSR(4, a_time, d_time, s_level, r_time, velocity, a_time*s_level);
 }
 
 void setup() {
-  //set callbacks.
-  MIDI.setHandleNoteOn(handleNoteOn);
-  MIDI.setHandleNoteOff(handleNoteOff);
+   //set callbacks
+   MIDI.setHandleNoteOn(handleNoteOn);
+   MIDI.setHandleNoteOff(handleNoteOff);
+  
+   //listen to all channels for change
+   MIDI.begin(MIDI_CHANNEL_OMNI);
 
-  //listen to all channels for change
-  MIDI.begin(MIDI_CHANNEL_OMNI);
-
-  // set input and output ports
- pinMode(ATTACK_TIME, INPUT);
- pinMode(DECAY_TIME, INPUT);
- pinMode(RELEASE_TIME, INPUT);
- pinMode(SUSTAIN_LEVEL, INPUT);
-
- pinMode(CV_OUT_VCO, OUTPUT);
- pinMode(CV_OUT_VCA, OUTPUT);
- pinMode(CV_OUT_VCF, OUTPUT);
- pinMode(SUSTAIN_VOLTAGE, OUTPUT);
+    // set input and output ports
+   pinMode(ATTACK_TIME, INPUT);
+   pinMode(DECAY_TIME, INPUT);
+   pinMode(RELEASE_TIME, INPUT);
+   pinMode(SUSTAIN_LEVEL, INPUT);
+  
+   pinMode(CV_OUT_VCO, OUTPUT);
+   pinMode(CV_OUT_VCA, OUTPUT);
+   pinMode(CV_OUT_VCF, OUTPUT);
+   pinMode(ADSR, OUTPUT);
 }
 
 
@@ -58,6 +125,7 @@ void loop() {
  MIDI.read();
  delay(100);
 }
+
 
 /**
  * Writes voltage to pin if voltage is between 255 and 0 which should be ensured by "byte" anyway
